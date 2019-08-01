ab5732a15b71090fd00ae17a43c5c5bac7183b43
AlexFBrito <slapyalex@gmail.com>
2019-08-01 08:39:45 -0300

    Acrescentada a mensagem de bom dia

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,9 @@
-package entidades;
+package aplicacao;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Bom dia!");
 
 	}
 
